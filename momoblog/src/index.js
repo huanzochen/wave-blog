4e22fc2d0546092b99fd24a694e4fefee04cc36c
g <fd>
2020-01-07 01:14:26 +0800

    tutroial 練習 & 將 UI 虛擬化

--- momoblog/src/index.js
+++ momoblog/src/index.js
@@ -2,11 +2,34 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 
+  class UI extends React.Component {
+    render() {
+      return (
+        <div class="container-fluid">
+          <div class="row">
+            <div id="main" class="col-9 "><h1 class="title display-3"><span class="badge badge-secondary">茉茉部落格</span></h1></div>
+            <div id="sidebar" class="col-3">
+              <div class="inner">
+                <nav id="menu">
+                  <ul> 
+                    <li class="list-group-item "><a class="sidebar_title list-group-item-action" href="https://www.google.com">註冊</a></li>
+                    <li class="list-group-item "><a class="sidebar_title list-group-item-action" href="https://www.google.com">登入</a></li>
+                    <li class="list-group-item "><a class="sidebar_title list-group-item-action" href="https://www.google.com">文章列表</a></li>
+                  </ul>
+                </nav>
+              </div>
+            </div>
+          </div>
+        </div>
+      )
+    }
+
+  }
 
 
-
-
+  
   ReactDOM.render(
+    <UI />,
     document.getElementById('root')
   );
 
--- tutorial/src/index.js
+++ tutorial/src/index.js
@@ -112,8 +112,139 @@ import './index.css';
   }
 
 
+  class Toggle extends React.Component {
+    constructor(props) {
+      super(props);
+      this.state = {isToggleOn: true};
+  
+      // 為了讓 `this` 能在 callback 中被使用，這裡的綁定是必要的：
+      this.handleClick = this.handleClick.bind(this);
+    }
+    handleClick() {
+      this.setState(state => ({
+        isToggleOn: !state.isToggleOn
+      }));
+    }
+    render() {
+      return (
+        <button onClick={this.handleClick}>
+          {this.state.isToggleOn ? 'ON' : 'OFF'}
+        </button>
+      );
+    }
+
+
+  }
+
+  function UserGreeting(props) {
+    return <h1>Welcome back!</h1>;
+  }
+  
+  function GuestGreeting(props) {
+    return <h1>Please sign up.</h1>;
+  }
+
+  function Greeting(props) {
+    const isLoggedIn = props.isLoggedIn;
+    if (isLoggedIn) {
+      return <UserGreeting />;
+    }
+    return <GuestGreeting />;
+  }
+
+  function LoginButton(props) {
+    return (
+      <button onClick={props.onClick}>
+        Login
+      </button>
+    );
+  }
+  
+  function LogoutButton(props) {
+    return (
+      <button onClick={props.onClick}>
+        Logout
+      </button>
+    );
+  }
+
+  class LoginControl extends React.Component {
+    constructor(props) {
+      super(props);
+      this.handleLoginClick = this.handleLoginClick.bind(this);
+      this.handleLogoutClick = this.handleLogoutClick.bind(this);
+      this.state = {isLoggedIn: false};
+    }
+  
+    handleLoginClick() {
+      this.setState({isLoggedIn: true});
+    }
+  
+    handleLogoutClick() {
+      this.setState({isLoggedIn: false});
+    }
+  
+    render() {
+      const isLoggedIn = this.state.isLoggedIn;
+      let button;
+  
+      if (isLoggedIn) {
+        button = <LogoutButton onClick={this.handleLogoutClick} />;
+      } else {
+        button = <LoginButton onClick={this.handleLoginClick} />;
+      }
+  
+      return (
+        <div>
+          <Greeting isLoggedIn={isLoggedIn} />
+          {button}
+        </div>
+      );
+    }
+  }
+
+  function WarningBanner(props) {
+    if (!props.warn) {
+      return null;
+    }
+  
+    return (
+      <div className="warning">
+        Warning!
+      </div>
+    );
+  }
+  
+  class Page extends React.Component {
+    constructor(props) {
+      super(props);
+      this.state = {showWarning: true};
+      this.handleToggleClick = this.handleToggleClick.bind(this);
+    }
+  
+    handleToggleClick() {
+      this.setState(state => ({
+        showWarning: !state.showWarning
+      }));
+    }
+  
+    render() {
+      return (
+        <div>
+          <WarningBanner warn={this.state.showWarning} />
+          <button onClick={this.handleToggleClick}>
+            {this.state.showWarning ? 'Hide' : 'Show'}
+          </button>
+        </div>
+      );
+    }
+  }
+
+
+
   ReactDOM.render(
-    <Clock />, 
+    //<Greeting isLoggedIn={false} />
+    <Page />,
     document.getElementById('root')
   );
 
