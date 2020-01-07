6107124e20b2ac7301aa301853d2d67175ea82ab
g <fd>
2020-01-08 02:02:51 +0800

    login & none login button

--- momoblog/src/index.js
+++ momoblog/src/index.js
@@ -1,33 +1,76 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+  class NoneLoginBtnList extends React.Component {
+    render() {
+      return (
+        <ul>
+          <li className="list-group-item"><a className="sidebar_title list-group-item-action" href="https://www.google.com">註冊</a></li>
+          <li className="list-group-item"><a className="sidebar_title list-group-item-action" href="https://www.google.com">登入</a></li>
+          <li className="list-group-item"><a className="sidebar_title list-group-item-action" href="https://www.google.com">文章列表</a></li>
+        </ul>
+      );
+    }
+  }
+
+  class LoginBtnList extends React.Component {
+    render() {
+      return (
+        <ul>
+          <li className="list-group-item"><a className="sidebar_title list-group-item-action" href="https://www.google.com">登出</a></li>
+          <li className="list-group-item"><a className="sidebar_title list-group-item-action" href="https://www.google.com">文章列表</a></li>
+        </ul>
+      );
+    }
+  }
+
+
+  class Member extends React.Component {
+    constructor(props) {
+      super(props);
+      this.state = {
+        isLogin : false
+      }
+    }
+    render() {
+      const isLogin = this.state.isLogin;
+      if (isLogin) {
+        return (
+          <LoginBtnList />
+        );
+      }
+      else {
+        return (
+          <NoneLoginBtnList />
+        );
+      }
+    }
+  } 
+
+
 
   class UI extends React.Component {
     render() {
       return (
         <div class="container-fluid">
           <div class="row">
-            <div id="main" class="col-9 "><h1 class="title display-3"><span class="badge badge-secondary">茉茉部落格</span></h1></div>
-            <div id="sidebar" class="col-3">
+            <div id="main" className="col-9"><h1 class="title display-3"><span className="badge badge-secondary">茉茉部落格</span></h1></div>
+            <div id="sidebar" className="col-3">
               <div class="inner">
                 <nav id="menu">
-                  <ul> 
-                    <li class="list-group-item "><a class="sidebar_title list-group-item-action" href="https://www.google.com">註冊</a></li>
-                    <li class="list-group-item "><a class="sidebar_title list-group-item-action" href="https://www.google.com">登入</a></li>
-                    <li class="list-group-item "><a class="sidebar_title list-group-item-action" href="https://www.google.com">文章列表</a></li>
-                  </ul>
+                  <Member />
                 </nav>
               </div>
             </div>
           </div>
         </div>
-      )
+      );
     }
 
   }
 
 
-  
+
   ReactDOM.render(
     <UI />,
     document.getElementById('root')
