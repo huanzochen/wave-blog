44a4fdc8ba463a2f297b8cee716bc5d462a745f8
tommy huang <tommy.huang@wavenet.com.tw>
2020-06-26 16:52:36 +0800

    update

--- momoblog/.eslintrc.js
+++ momoblog/.eslintrc.js
@@ -3,15 +3,5 @@ module.exports = {
     "rules": {
         // enable additional rules
         "indent": ["error", 2],
-        "linebreak-style": ["error", "unix"],
-        "quotes": ["error", "double"],
-        "semi": ["error", "always"],
-
-        // override default options for rules from base configurations
-        "comma-dangle": ["error", "always"],
-        "no-cond-assign": ["error", "always"],
-
-        // disable rules from base configurations
-        "no-console": "off",
     }
 }
