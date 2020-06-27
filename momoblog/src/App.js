3e8e0bdd9d8de3b73e4408066a1639e3c535063d
tommy huang <tommy.huang@wavenet.com.tw>
2020-06-27 11:25:41 +0800

    code beautify

--- momoblog/src/App.js
+++ momoblog/src/App.js
@@ -11,144 +11,144 @@ import webhookURL from './util/config/webhookURL';
 
 
 const RouteFallback = (props) => { 
-    console.log('route fallback with location: ', props.location); 
-    return <Redirect to={{
-      pathname: '/',
-      from: props.location
-    }} /> 
+  console.log('route fallback with location: ', props.location); 
+  return <Redirect to={{
+    pathname: '/',
+    from: props.location
+  }} /> 
 }
 
 export default class App extends React.Component {
-    constructor() {
-      super();
-      this.state = {
-        loggedInStatus: "NOT_LOGGED_IN",
-        username: {}
-      };
-      this.handleLogin = this.handleLogin.bind(this);
-      this.handleLogout = this.handleLogout.bind(this);
-      this.handleLogoutClick = this.handleLogoutClick.bind(this);
-    }
+  constructor() {
+    super();
+    this.state = {
+      loggedInStatus: "NOT_LOGGED_IN",
+      username: {}
+    };
+    this.handleLogin = this.handleLogin.bind(this);
+    this.handleLogout = this.handleLogout.bind(this);
+    this.handleLogoutClick = this.handleLogoutClick.bind(this);
+  }
     
-    checkLoginStatus() {
-      axios.get( webhookURL.url + '/api/logged_in', { withCredentials:true})
+  checkLoginStatus() {
+    axios.get( webhookURL.url + '/api/logged_in', { withCredentials:true})
       .then(response => {
-          if (response.data.logged_in && this.state.loggedInStatus === "NOT_LOGGED_IN"){
-            this.setState({
-                loggedInStatus: "LOGGED_IN",
-                username: response.data.username
-              });
-          }
-          else if (!response.data.logged_in && this.state.loggedInStatus === "LOGGED_IN") {
-            this.setState({
-                loggedInStatus: "NOT_LOGGED_IN",
-                username: {}
-              });
-          }
-          console.log("logged in?", response);
+        if (response.data.logged_in && this.state.loggedInStatus === "NOT_LOGGED_IN"){
+          this.setState({
+            loggedInStatus: "LOGGED_IN",
+            username: response.data.username
+          });
+        }
+        else if (!response.data.logged_in && this.state.loggedInStatus === "LOGGED_IN") {
+          this.setState({
+            loggedInStatus: "NOT_LOGGED_IN",
+            username: {}
+          });
+        }
+        console.log("logged in?", response);
       })
       .catch(error => {
-          console.log("check login error", error);
+        console.log("check login error", error);
       });
       
-    }
+  }
 
-    componentDidMount() {
-        this.checkLoginStatus();
-    }
+  componentDidMount() {
+    this.checkLoginStatus();
+  }
     
 
-    handleLogin(data) {
-      this.setState({
-        loggedInStatus: "LOGGED_IN",
-        username: data.username
-      });
-    }
+  handleLogin(data) {
+    this.setState({
+      loggedInStatus: "LOGGED_IN",
+      username: data.username
+    });
+  }
 
-    handleLogout() {
-        this.setState({
-          loggedInStatus: "NOT_LOGGED_IN",
-          username: {}
-        });
-    }
+  handleLogout() {
+    this.setState({
+      loggedInStatus: "NOT_LOGGED_IN",
+      username: {}
+    });
+  }
 
 
-    handleLogoutClick() {
-        axios.get( webhookURL.url + '/api/logout/submit', { withCredentials: true})
-         .then((response) => {
-            this.handleLogout();
-         })
-         .catch(error => {
-           console.log("logout error", error);
-         })
-    }
+  handleLogoutClick() {
+    axios.get( webhookURL.url + '/api/logout/submit', { withCredentials: true})
+      .then((response) => {
+        this.handleLogout();
+      })
+      .catch(error => {
+        console.log("logout error", error);
+      })
+  }
 
-    render() {
-      return (
-        <BrowserRouter>
-          <Switch>
-              {routes.map((route,i) => {
-                const{ path, exact, routes } = route;
-                return (
-                  <Route 
-                  key={i}
-                  path={path} 
-                  exact={exact} 
-                  render={(routeProps) => (
-                    <route.component routes={routes} {...routeProps}
-                        handleLogin={this.handleLogin}
-                        handleLogout={this.handleLogout}
-                        handleLogoutClick={this.handleLogoutClick}
-                        loggedInStatus={this.state.loggedInStatus}
-                        username={this.state.username}
-                    />
-                  )} />
-                )
-              })}
-          </Switch>
-        </BrowserRouter>
-      );
-    }
+  render() {
+    return (
+      <BrowserRouter>
+        <Switch>
+          {routes.map((route,i) => {
+            const{ path, exact, routes } = route;
+            return (
+              <Route 
+                key={i}
+                path={path} 
+                exact={exact} 
+                render={(routeProps) => (
+                  <route.component routes={routes} {...routeProps}
+                    handleLogin={this.handleLogin}
+                    handleLogout={this.handleLogout}
+                    handleLogoutClick={this.handleLogoutClick}
+                    loggedInStatus={this.state.loggedInStatus}
+                    username={this.state.username}
+                  />
+                )} />
+            )
+          })}
+        </Switch>
+      </BrowserRouter>
+    );
+  }
 }
 
 //之後可以分開來寫
 const routes = [
-{
+  {
     path: '/',
     component: Homepage,
     exact: true,
     breadcrumbName: 'Homepage'
-},
-{
+  },
+  {
     path: '/register',
     component: RegisterAll,
     exact: true,
     breadcrumbName: 'Register'
-},
-{
+  },
+  {
     path: '/login',
     component: LoginAll,
     exact: true,
     breadcrumbName: 'Login'
-},
-{
+  },
+  {
     path: '/Articlepad',
     component: Articlepad,
     exact: true,
     breadcrumbName: 'Articlepad'
-},
-{
-  path: '/ArticleDetail',
-  component: ArticleDetail,
-  exact: false,
-  breadcrumbName: 'Index'
-},
-{
+  },
+  {
+    path: '/ArticleDetail',
+    component: ArticleDetail,
+    exact: false,
+    breadcrumbName: 'Index'
+  },
+  {
     path: '/',
     component: RouteFallback,
     exact: false,
     breadcrumbName: 'Index'
-}
+  }
 ];
 
  
--- momoblog/src/auth/Login.js
+++ momoblog/src/auth/Login.js
@@ -3,96 +3,96 @@ import axios from 'axios';
 import webhookURL from '../util/config/webhookURL';
 
 export default class Login extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            username: "",
-            password: "",
-            loginErrors: ""
+  constructor(props) {
+    super(props);
+    this.state = {
+      username: "",
+      password: "",
+      loginErrors: ""
+    }
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({
+      [event.target.name]: event.target.value
+    });
+    console.log("內容有變動");
+  }
+
+  handleSubmit(event) {
+    const {username, password} = this.state;
+    axios.post( webhookURL.url + '/api/login/submit', {
+      user: {
+        username: username,
+        password: password
+      }
+    },
+    {
+      withCredentials: true
+    },
+    )
+      .then(response => {
+        if (response.data.isLoggedIn){
+          console.dir("登入成功!");
+          //localStorage.setItem('usertoken', response.data.token);
+          this.setState({loginErrors: response.data.errorText});
+          console.log("登入結果!", response);
+          this.props.handleSuccessfulAuth(response.data);
         }
-        this.handleSubmit = this.handleSubmit.bind(this);
-        this.handleChange = this.handleChange.bind(this);
-    }
-
-    handleChange(event) {
-        this.setState({
-            [event.target.name]: event.target.value
-        });
-        console.log("內容有變動");
-    }
-
-    handleSubmit(event) {
-        const {username, password} = this.state;
-        axios.post( webhookURL.url + '/api/login/submit', {
-            user: {
-                username: username,
-                password: password
-            }
-        },
-        {
-            withCredentials: true
-        },
-        )
-        .then(response => {
-            if (response.data.isLoggedIn){
-                console.dir("登入成功!");
-                //localStorage.setItem('usertoken', response.data.token);
-                this.setState({loginErrors: response.data.errorText});
-                console.log("登入結果!", response);
-                this.props.handleSuccessfulAuth(response.data);
-            }
-            else if (!response.data.isLoggedIn){
-                this.setState({loginErrors:response.data.errorText});
-            }
-        })
-        .catch(error => {
-            console.dir("登入失敗!", error);
-        })
-        event.preventDefault();
-    }
+        else if (!response.data.isLoggedIn){
+          this.setState({loginErrors:response.data.errorText});
+        }
+      })
+      .catch(error => {
+        console.dir("登入失敗!", error);
+      })
+    event.preventDefault();
+  }
     
-    render() {
-        return (
-            <div className="article">
-                <div className="article_title">
-                    <form onSubmit={this.handleSubmit} >
-                        <div className="row validate">
-                            <p>帳號: </p>
-                            <input type="text" 
-                                name="username" 
-                                placeholder="Username" 
-                                value={this.state.username} 
-                                onChange={this.handleChange}  
-                                className="form-control" 
-                                autoComplete="username" 
-                                maxLength="16" 
-                                required
-                                />
-                        </div>
-                        <div className="row validate">
-                            <p>密碼:</p>
-                            <input type="password" 
-                                name="password" 
-                                placeholder="Password" 
-                                value={this.state.password} 
-                                onChange={this.handleChange} 
-                                className="form-control" 
-                                autoComplete="new-password" 
-                                maxLength="16" 
-                                required 
-                            />
-                        </div>
-                        <div className="row justify-content-between validate">
-                            <div className="col-5">
-                                <button type="submit" className="btn btn-light">登入</button>
-                            </div>
-                            <div className="col-7">
-                                <p className="error_code">{this.state.loginErrors}</p>
-                            </div>
-                        </div>
-                    </form>
-                </div>
+  render() {
+    return (
+      <div className="article">
+        <div className="article_title">
+          <form onSubmit={this.handleSubmit} >
+            <div className="row validate">
+              <p>帳號: </p>
+              <input type="text" 
+                name="username" 
+                placeholder="Username" 
+                value={this.state.username} 
+                onChange={this.handleChange}  
+                className="form-control" 
+                autoComplete="username" 
+                maxLength="16" 
+                required
+              />
             </div>
-        );
-    }
+            <div className="row validate">
+              <p>密碼:</p>
+              <input type="password" 
+                name="password" 
+                placeholder="Password" 
+                value={this.state.password} 
+                onChange={this.handleChange} 
+                className="form-control" 
+                autoComplete="new-password" 
+                maxLength="16" 
+                required 
+              />
+            </div>
+            <div className="row justify-content-between validate">
+              <div className="col-5">
+                <button type="submit" className="btn btn-light">登入</button>
+              </div>
+              <div className="col-7">
+                <p className="error_code">{this.state.loginErrors}</p>
+              </div>
+            </div>
+          </form>
+        </div>
+      </div>
+    );
+  }
 }
