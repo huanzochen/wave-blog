4e7ee072009a02cbdd2f6b38a75191145f32670e
tommy.huang <cy94295@gmail.com>
2021-01-20 12:13:38 +0800

    delete unused file

--- momobackend/util/config/webhookURL.js
+++
@@ -1,6 +0,0 @@
-require('dotenv').config()
-
-exports = module.exports = {
-    url: process.env.WEB_APP_URL
-}
-/**  webhookURL連結 此url指向前端 */
--- momoblog/src/util/config/webhookURL.js
+++
@@ -1,4 +0,0 @@
-exports = module.exports = {
-  url: 'http://localhost:3200',
-}
-/**  webhookURL連結 此url指向後端 */
